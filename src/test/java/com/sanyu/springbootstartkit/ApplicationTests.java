8e9298079b7156f884883ccd54c9ed189f2e1b54
devops <devops@mygithub.com>
2018-09-23 15:04:59 +0800

    docker-maven-plugin 插件 实现 Docker 部署: mvn clean package docker:build

--- src/test/java/com/sanyu/springbootstartkit/ApplicationTests.java
+++ src/test/java/com/sanyu/springbootstartkit/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sanyu.spring.bootstartkit;
+package com.sanyu.springbootstartkit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootstartkitApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
